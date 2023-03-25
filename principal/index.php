de6e4160db0a19e45d0988dfaa173dc40c527c8f
xt4568 <xt4568@rimac.com.pe>
2023-03-25 11:30:23 -0500

    .

--- principal/index.php
+++ principal/index.php
@@ -1,8 +1,11 @@
-<?php include('session_user.php');
-require_once('../conexion.php');
-require_once('../titulo_sist.php');
+<?php 
 echo("hola 4");
 echo($_SESSION['codigo_user']);
+
+include('session_user.php');
+require_once('../conexion.php');
+require_once('../titulo_sist.php');
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
