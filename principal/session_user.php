081391b17a8e79c9817aef592f27e03221bfb18d
xt4568 <xt4568@rimac.com.pe>
2023-03-25 11:12:35 -0500

    .

--- principal/session_user.php
+++ principal/session_user.php
@@ -5,8 +5,8 @@ error_log("Caducidad: ".$maxlifetime);
 // Verificar si ya hay una sesión activa
 if (session_status() !== PHP_SESSION_ACTIVE) {
 
-    define('SEG_RAIZ',   "/novafarma");
-    session_set_cookie_params(86400);
+    define('SEG_RAIZ',   "/");
+    //session_set_cookie_params(86400);
     ini_set('session.cookie_lifetime',86400);
     ini_set('session.gc_maxlifetime', 86400);
     session_start();
