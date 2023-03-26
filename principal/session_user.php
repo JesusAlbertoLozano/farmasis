904f4f3c2e0c31f9fa77b8b201847ebbce446ba2
xt4568 <xt4568@rimac.com.pe>
2023-03-26 12:52:57 -0500

    .

--- principal/session_user.php
+++ principal/session_user.php
@@ -1,6 +1,6 @@
 <?php 
 $maxlifetime = ini_get("session.gc_maxlifetime");
-error_log("Caducidad: ".$maxlifetime);
+//error_log("Caducidad: ".$maxlifetime);
 
 // Verificar si ya hay una sesión activa
 if (session_status() !== PHP_SESSION_ACTIVE) {
@@ -9,13 +9,13 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
     //session_set_cookie_params(86400);
     //ini_set('session.cookie_lifetime',86400);
     //ini_set('session.gc_maxlifetime', 86400);
-    //session_start();
+    session_start();
 } else {
     define('SEG_RAIZ', '/principal');
 }
 
 $maxlifetime = ini_get("session.gc_maxlifetime");
-error_log("Caducidad despues: ".$maxlifetime);
+//error_log("Caducidad despues: ".$maxlifetime);
 
 $usuario    =(isset($_SESSION['codigo_user']) ? $_SESSION['codigo_user'] : "");
 $resolucion  =(isset($_SESSION['resolucion']) ? $_SESSION['resolucion'] : "");
@@ -24,7 +24,7 @@ $resolucion  =(isset($_SESSION['resolucion']) ? $_SESSION['resolucion'] : "");
 
 if ($usuario == "") 
 {
-    error_log("Termino la sesi��n ");
+    //error_log("Termino la sesi��n ");
     $url = SEG_RAIZ."/index.php";
     header('Location: '.$url);
     exit;
