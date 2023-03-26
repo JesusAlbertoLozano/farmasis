4cc471d8bd8c9552070de1f89b0855a99f948337
xt4568 <xt4568@rimac.com.pe>
2023-03-26 14:29:28 -0500

    .

--- principal/session_user.php
+++ principal/session_user.php
@@ -1,10 +1,10 @@
 <?php 
 $maxlifetime = ini_get("session.gc_maxlifetime");
-//error_log("Caducidad: ".$maxlifetime);
+print("Caducidad: ".$maxlifetime);
 
 // Verificar si ya hay una sesión activa
 if (session_status() !== PHP_SESSION_ACTIVE) {
-
+    print("la sesion no estaba activa");
     define('SEG_RAIZ', '..');
     //session_set_cookie_params(86400);
     //ini_set('session.cookie_lifetime',86400);
@@ -15,10 +15,10 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 }
 
 $maxlifetime = ini_get("session.gc_maxlifetime");
-//error_log("Caducidad despues: ".$maxlifetime);
+print("Caducidad: ".$maxlifetime);
 
-$usuario    =(isset($_SESSION['codigo_user']) ? $_SESSION['codigo_user'] : "");
-$resolucion  =(isset($_SESSION['resolucion']) ? $_SESSION['resolucion'] : "");
+$usuario    =(!isset($_SESSION['codigo_user']) ? $_SESSION['codigo_user'] : "");
+$resolucion  =(!isset($_SESSION['resolucion']) ? $_SESSION['resolucion'] : "");
 
 
 
